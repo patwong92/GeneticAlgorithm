3da22b76d608f9b1f593d5aa56391653978ccc87
Patrick <pkhwong.92@gmail.com>
2019-11-12 15:41:15 -0800

    My first commit!

---
+++ cmake-build-debug/CMakeFiles/GeneticAlgorithm.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GeneticAlgorithm.dir/main.cpp.o"
+  "GeneticAlgorithm.pdb"
+  "GeneticAlgorithm.exe"
+  "libGeneticAlgorithm.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GeneticAlgorithm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
